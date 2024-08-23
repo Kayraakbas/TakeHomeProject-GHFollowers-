cd2d968869bc9fc0e2851d9dfb408202cfb2217c
Kayraakbas <74239510+Kayraakbas@users.noreply.github.com>
2024-08-23 13:50:46 +0300

    Initial Commit

---
+++ TakeHomeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TakeHomeProject
+//
+//  Created by Kayra Akbaş on 23.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
